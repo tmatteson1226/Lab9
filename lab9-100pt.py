8abf6e67d5db03182d5d429ab8aaf8686ba6f1ae
tmatteson1226 <s631741@student.roundrockisd.org>
2014-10-17 15:35:36 -0500

    Finished 100pt
    100pt done

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,34 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+
+
+keepGoing = True
+while (keepGoing == True):
+    print 'how hot are you?'
+    t = int(raw_input())
+    print 'have you been to africa? y or n'
+    a = raw_input()
+    print 'have you been sick in the last 24 hours? y or n'
+    s = raw_input()
+    
+    if t > 105:
+        keepGoing = False
+        print 'you have a sickness'
+    elif t >= 102 and s == "y":
+        keepGoing = False
+        print 'you have a sickness'
+    elif (t >= 100 or s == 'y') and a == 'y':
+        keepGoing = False
+        print 'you have a sickness'
+    else:
+        print 'you probably do not have ebola'
+    
+    print 'are there any more patients? y or n'
+    p = raw_input()
+    if p == 'n':
+        keepGoing = False
+    elif p == 'y':
+        keepGoing = True
+    else:
+        print 'please input valid answer'
